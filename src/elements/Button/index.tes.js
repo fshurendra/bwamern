96f6babf8546a44033fb693a4aed2a4a3368d35f
fshurendra <fshurendra@gmail.com>
2020-09-20 23:54:51 +0700

    Finish button

--- src/elements/Button/index.tes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import Button from './index'
-
-test("Should not allowed click button if isDisabled is present", () => {
-    const {container} = render(<Button isDisabled></Button>)
-
-    expect(container.querySelector('span.disabled')).toBeInTheDocument()
-})
